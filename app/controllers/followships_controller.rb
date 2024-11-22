dd710fb846589752d3e9a0d4722eff6f3ed1fadf
Rido Atmanto <ridoatmanto@gmail.com>
2024-11-22 14:57:43 +0700

    Refactor controller

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -4,27 +4,31 @@ class FollowshipsController < ApplicationController
   end
 
   def create
-    render json: Followship.create(following_params)
+    existing = Followship.where(follower_id: params[:user][:follower_id], followee_id: params[:user][:followee_id]).first
+    
+    return render json: Followship.create(following_params) if existing.nil?
+
+    render status: 403, json: { message: "Already followed!" }
   end
 
   def show
-    render json: Followship.followed(params[:id])
+    render json: Followship.followed(params[:id]), include: {user: {only: :name}}
   end
 
   def destroy
-    render json: Followship.where({follower_id: params[:id], followee_id: params[:user][:follower_id]}).delete_all
+    render json: Followship.where({follower_id: params[:id], followee_id: params[:user][:followee_id]}).delete_all
   end
 
   def sleep_duration
     # followed user sleep record in a week ago
     ids = Followship.followed_ids(params[:id])
 
-    render json: SleepingTime.week_ago(ids)
+    render json: SleepingTime.week_ago(ids), include: {user: {only: :name}}
   end
 
   private
 
   def following_params
-    params.require(:user).permit(:followee_id, :follower_id)
+    params.require(:user).permit(:follower_id, :followee_id)
   end
 end
