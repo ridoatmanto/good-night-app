dbda39c03432c52b1b3c4e769f4a4f4bde9d4c0f
Rido Atmanto <ridoatmanto@gmail.com>
2024-11-22 14:58:38 +0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :followships
 
-  post '/clock-in', to: 'user_clocks#clock_in'
-  post '/clock-out', to: 'user_clocks#clock_out'
-  get '/followed-user-sleep-duration/(:id)', to: 'users#sleep_duration'
+  get '/users', to: 'users#index'
+  get '/users/(:id)', to: 'users#show'
+  
+  post '/clock-in', to: 'sleeping_times#clock_in'
+  post '/clock-out', to: 'sleeping_times#clock_out'
+  get '/followed-user-sleep-duration/(:id)', to: 'followships#sleep_duration'
 
   get '/', to: proc { [200, {}, ['']] }
 end
